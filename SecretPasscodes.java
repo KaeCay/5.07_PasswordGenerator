79f740ec2014dba0744878f3c9002b7ca2a3664d
Kayla <xxkaycae@gmail.com>
2018-11-22 11:24:23 -0600

    Update SecretPasscodes.java
    Updates:
    
    - program ends (stops all prompts) after #6 / Quit is selected

--- SecretPasscodes.java
+++ SecretPasscodes.java
@@ -33,91 +33,96 @@ public class SecretPasscodes
        File genPass = new File ("generatedPass.txt");
        Random charGen = new Random();
        PrintWriter charWrite = new PrintWriter (genPass);
+       boolean quit = false;
        int choice;
        int passLength;
        
-       
-       //Output
-       System.out.print("Select a category (1-6): ");
-       choice = in.nextInt();
-       
-       while (!(choice <= 6))   //Makes sure input matches the menu choices
+       while (quit == false)
        {
-           System.out.println(); 
-           System.out.println("Entry invalid");
-           System.out.print("Select a category (1-6): ");
-           choice = in.nextInt();
-        }
-        
-       System.out.println();
-       System.out.print("How long will your password be? (Min. 6): ");
-       passLength = in.nextInt();
-       
-       while (!(passLength >= 6))   //Makes sure password length is at least 6
-       {
-           System.out.println(); 
-           System.out.println("Password not long enough");
-           System.out.print("How long will your password be? (Min. 6): ");
-           passLength = in.nextInt();
-        }
-        
-       if (choice == 1) //All lowercase generator
-       {
-           for (int charCount = 0; charCount < passLength; charCount++)
-           {
-               int character = charGen.nextInt((122 - 97) + 1) + 97;
-               char entry = (char)character;
-               charWrite.print(entry);
-           }
-       }
-       else if (choice == 2) //All uppercase generator
-       {
-          for (int charCount = 0; charCount < passLength; charCount++)
-          {
-              int character = charGen.nextInt((90 - 65)+ 1) + 65;
+         //Output
+         System.out.print("Select a category (1-6): ");
+         choice = in.nextInt();
+         
+         while (!(choice <= 6))   //Makes sure input matches the menu choices
+         {
+             System.out.println(); 
+             System.out.println("Entry invalid");
+             System.out.print("Select a category (1-6): ");
+             choice = in.nextInt();
+          }
+
+         if (choice == 6) //Quit
+         {
+            break;
+         }
+          
+         System.out.println();
+         System.out.print("How long will your password be? (Min. 6): ");
+         passLength = in.nextInt();
+         
+         while (!(passLength >= 6))   //Makes sure password length is at least 6
+         {
+             System.out.println(); 
+             System.out.println("Password not long enough");
+             System.out.print("How long will your password be? (Min. 6): ");
+             passLength = in.nextInt();
+          }
+          
+         if (choice == 1) //All lowercase generator
+         {
+             for (int charCount = 0; charCount < passLength; charCount++)
+             {
+                 int character = charGen.nextInt((122 - 97) + 1) + 97;
+                 char entry = (char)character;
+                 charWrite.print(entry);
+             }
+         }
+         else if (choice == 2) //All uppercase generator
+         {
+            for (int charCount = 0; charCount < passLength; charCount++)
+            {
+                int character = charGen.nextInt((90 - 65)+ 1) + 65;
+                char entry = (char)character;
+                charWrite.print(entry);
+            } 
+         }
+         else if (choice == 3) //All numbers generator
+         {
+            for (int charCount = 0; charCount < passLength; charCount++)
+            {
+              int character = charGen.nextInt((57 - 48)+ 1) + 48;
               char entry = (char)character;
               charWrite.print(entry);
-          } 
-       }
-       else if (choice == 3) //All numbers generator
-       {
-          for (int charCount = 0; charCount < passLength; charCount++)
-          {
-            int character = charGen.nextInt((57 - 48)+ 1) + 48;
-            char entry = (char)character;
-            charWrite.print(entry);
-          }
-       }
-       else if (choice == 4) //Lowercase, numbers generator
-       {
-          for (int charCount = 0; charCount < passLength; charCount++)
-          {
-            int character = charGen.nextInt((122 - 97) + 1) + 97;  //lowercase letter
-            char entry = (char)character;
-            charWrite.print(entry);
-            charCount++; //count extra character
-
-            character = charGen.nextInt((57 - 48)+ 1) + 48;  //number
-            entry = (char)character;
-            charWrite.print(entry);
-          }
-       }
-       else if (choice == 5) //Uppercase, numbers generator
-       {
-        for (int charCount = 0; charCount < passLength; charCount++)
-          {
-              int character = charGen.nextInt((90 - 65)+ 1) + 65; //upeprcase letter
+            }
+         }
+         else if (choice == 4) //Lowercase, numbers generator
+         {
+            for (int charCount = 0; charCount < passLength; charCount++)
+            {
+              int character = charGen.nextInt((122 - 97) + 1) + 97;  //lowercase letter
               char entry = (char)character;
-              charWrite.print(entry); 
+              charWrite.print(entry);
               charCount++; //count extra character
 
               character = charGen.nextInt((57 - 48)+ 1) + 48;  //number
               entry = (char)character;
               charWrite.print(entry);
-          } 
-       }
+            }
+         }
+         else if (choice == 5) //Uppercase, numbers generator
+         {
+            for (int charCount = 0; charCount < passLength; charCount++)
+            {
+                int character = charGen.nextInt((90 - 65)+ 1) + 65; //upeprcase letter
+                char entry = (char)character;
+                charWrite.print(entry); 
+                charCount++; //count extra character
 
-
+                character = charGen.nextInt((57 - 48)+ 1) + 48;  //number
+                entry = (char)character;
+                charWrite.print(entry);
+            } 
+         }
 
        charWrite.close();
 
@@ -132,6 +137,13 @@ public class SecretPasscodes
        System.out.println();
        System.out.println("Here is your new password!");
        System.out.println(password);
+       System.out.println();
+     }
 
+     System.out.println();
+     System.out.println("-------------------------");
+     System.out.println("|    Password Generator |");
+     System.out.println("|           End         |");
+     System.out.println("-------------------------");  
     }
 }
